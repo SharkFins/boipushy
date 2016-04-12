31b99cccfb7c8c1f1b909d72a9eeb8b3b07e3c42
SharkFins <SharkFins@users.noreply.github.com>
2016-04-13 00:20:47 +0100

    Changed for own use
    #Changed so only functions can be bound to a key
    #Added ID (0 or nil if keyboard, otherwise is the controller ID) so that keys and functions can be bound to specific controllers
    #Changed action so you either put "pressed", "released", "moved" or "held" depending when you want it to be called
    #Added gamestate so that you can have different function calls for the same key for different states.
    #Rewrote the way functions are called
    #Added modifiers (Numlock, capslock, scrolllock, rshift, lshift, lalt, ralt, lctrl, rctrl, lgui, rgui, mode)

--- Input.lua
+++ Input.lua
@@ -1,10 +1,14 @@
 local input_path = (...):match('(.-)[^%.]+$') .. '.'
 local Input = {}
+local self = {}
 Input.__index = Input
 
-local major, minor, rev = love.getVersion()
-love_version = major .. '.' .. minor .. '.' .. rev
-
+-- Local references
+  local pairs = pairs
+  local assert = assert
+  local next = next
+	local setmetatable = setmetatable
+  
 Input.all_keys = {
     " ", "return", "escape", "backspace", "tab", "space", "!", "\"", "#", "$", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "0", "1", "2", "3", "4",
     "5", "6", "7", "8", "9", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "", "`", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
@@ -15,30 +19,77 @@ Input.all_keys = {
     "alterase", "sysreq", "cancel", "clear", "prior", "return2", "separator", "out", "oper", "clearagain", "thsousandsseparator", "decimalseparator", "currencyunit",
     "currencysubunit", "lctrl", "lshift", "lalt", "lgui", "rctrl", "rshift", "ralt", "rgui", "mode", "audionext", "audioprev", "audiostop", "audioplay", "audiomute",
     "mediaselect", "brightnessdown", "brightnessup", "displayswitch", "kbdillumtoggle", "kbdillumdown", "kbdillumup", "eject", "sleep", "mouse1", "mouse2", "mouse3",
-    "mouse4", "mouse5", "wheelup", "wheeldown", "fdown", "fup", "fleft", "fright", "back", "guide", "start", "leftstick", "rightstick", "l1", "r1", "l2", "r2", "dpup",
+    "mouse4", "mouse5", "wheelup", "wheeldown", "wheelleft", "wheelright", "fdown", "fup", "fleft", "fright", "back", "guide", "start", "leftstick", "rightstick", "l1", "r1", "l2", "r2", "dpup",
     "dpdown", "dpleft", "dpright", "leftx", "lefty", "rightx", "righty",
 }
 
+Input.modifiers = {'numlock', 'capslock', 'scrolllock', 'rshift', 'lshift', 'rctrl', 'lctrl', 'ralt', 'lalt', 'rgui', 'lgui', 'mode'}
+
+local copy = function(t1)
+    local out = {}
+    for k, v in pairs(t1) do out[k] = v end
+    return out
+end
+
+local function updateJoysticks()
+  local joysticks = love.joystick.getJoysticks()
+  for k, v in pairs(joysticks) do
+    if k <= 4 and v:isGamepad() then
+      self.joysticks_to_ids[v] = k
+    end
+  end
+end
+
+-- Modifier must be input as some actions won't use modifiers e.g. mouse movement
+local function executeFunction(action, id, key, modifier, ...)
+    local binding = self.binds[action]
+    if binding[self.currentstate] ~= nil and binding[self.currentstate][id] ~=nil then
+      local idbinding = binding[self.currentstate][id]
+      if idbinding ~= nil then
+        if modifier == nil then
+          if idbinding[key] ~= nil then
+            idbinding[key](...)
+          end
+        else
+          if idbinding[modifier] ~= nil and idbinding[modifier][key] ~= nil then
+            idbinding[modifier][key](...)
+          end
+        end
+      end
+    end
+    if modifier == nil then
+      if self.binds[action]["all"][id][key] ~= nil then
+        self.binds[action]["all"][id][key](...)
+      end
+    else
+      if self.binds[action]["all"][id][modifier] ~= nil and self.binds[action]["all"][id][modifier][key] ~= nil then
+        self.binds[action]["all"][id][modifier][key](...)
+      end
+    end
+end
+
+local function setState(id, key, value)
+  self.state[id][key] = value
+end
+
+--Use this to do Alt+Y etc, remember to set to blank after
+-- Will only work if it's a modifier
+local function setModifier(key)
+  if key == nil or Input.modifiers[key] ~= nil then
+    self.currentmodifier = key
+  end
+end
+
 function Input.new()
-    local self = {}
-
-    if not love.load then error("Input instance must be created inside love.load") end
-
-    -- Previous and current key down state
-    self.prev_state = {}
-    self.state = {}
-
-    -- self.binds[action] has a list of keys that correspond to this action
-    self.binds = {}
-
-    -- self.functions[key] points to the action that this key activates
-    self.functions = {}
-
-    -- Gamepads... currently only supports 1 gamepad, adding support for more is not that hard, just lazy.
-    self.joysticks = love.joystick.getJoysticks()
-
-    -- Register callbacks automagically
-    local callbacks = {'keypressed', 'keyreleased', 'mousepressed', 'mousereleased', 'gamepadpressed', 'gamepadreleased', 'gamepadaxis', 'wheelmoved', 'update'}
+    self.joysticks_to_ids = {}
+    self.joystick_axis_values = {[0]={}, [1]={}, [2]={}, [3]={}, [4]={}}
+    self.ids = {[0]={}, [1]={}, [2]={}, [3]={}, [4]={}} -- Keyboard + Mouse and 4 Joysticks
+    self.state = {[0]={}, [1]={}, [2]={}, [3]={}, [4]={}}  -- Button State
+    self.currentstate = "all" -- Game State
+    self.currentmodifier = nil -- Keyboard Modifer
+    self.binds = {["pressed"] = {[self.currentstate] = copy(self.ids)}, ["moved"] = {[self.currentstate] = copy(self.ids)}, ["released"] = {[self.currentstate] = copy(self.ids)}, ["held"] = {[self.currentstate] = copy(self.ids)}}
+    
+    local callbacks = {'keypressed', 'keyreleased', 'mousepressed', 'mousereleased', 'mousemoved', 'joystickadded', 'joystickremoved', 'gamepadpressed', 'gamepadreleased', 'gamepadaxis', 'wheelmoved', 'update'}
     local old_functions = {}
     local empty_function = function() end
     for _, f in ipairs(callbacks) do
@@ -48,140 +99,213 @@ function Input.new()
             self[f](self, ...)
         end
     end
-
+    
     return setmetatable(self, Input)
 end
 
-function Input:bind(key, action)
-    if type(action) == 'function' then self.functions[key] = action; return end
-    if not self.binds[action] then self.binds[action] = {} end
-    table.insert(self.binds[action], key)
-end
-
-function Input:pressed(action)
-    if action then
-        for _, key in ipairs(self.binds[action]) do
-            if self.state[key] and not self.prev_state[key] then
-                return true
-            end
-        end
-
+--action can be pressed, released, moved or held
+--id is used to detect different controllers, 0 is for keyboard
+--e.g bind("s", "moved", "game", 0, game:whatever(val))
+--if gamestate == nil then the function will be called no matter the state
+--
+function Input:bind(key, action, gamestate, id, f)
+  assert(type(f) == 'function', "f is not a function")
+  if Input.modifiers[key] == nil then
+    if gamestate == nil then gamestate = "all" end
+    if self.joysticks_to_ids[id] ~= nil then id = self.joysticks_to_ids[id] else id = 0 end
+    if id == nil then id = 0 end
+    if self.binds[action][gamestate] == nil then self.binds[action][gamestate] = {} end
+    if self.binds[action][gamestate][id] == nil then self.binds[action][gamestate][id] = {} end
+    if self.currentmodifier == nil then
+      self.binds[action][gamestate][id][key] = f
     else
-        for _, key in ipairs(Input.all_keys) do
-            if self.state[key] and not self.prev_state[key] then
-                if self.functions[key] then
-                    self.functions[key]()
-                end
-            end
-        end
+      if self.binds[action][gamestate][id][self.currentmodifier] == nil then
+        self.binds[action][gamestate][id][self.currentmodifier] = {}
+      end
+      self.binds[action][gamestate][id][self.currentmodifier][key] = f
     end
+    return true -- Binding successful
+  end
+  return false -- Trying to bind to a modifier, try again with new key
 end
 
-function Input:released(action)
-    for _, key in ipairs(self.binds[action]) do
-        if self.prev_state[key] and not self.state[key] then
-            return true
-        end
+function Input:unbind(key, action, gamestate, id)
+  if Input.modifiers[key] == nil then
+    if gamestate == nil then gamestate = "all" end
+    if id == nil then id = 0 end
+    if self.currentmodifier == nil then
+      self.binds[action][gamestate][id][key] = nil
+    else
+      self.binds[action][gamestate][id][self.currentmodifier][key] = nil
     end
-end
-
-local key_to_button = nil
-if love_version == '0.9.1' or love_version == '0.9.2' then
-    key_to_button = {mouse1 = 'l', mouse2 = 'r', mouse3 = 'm', wheelup = 'wu', wheeldown = 'wd', mouse4 = 'x1', mouse5 = 'x2'}
-else key_to_button = {mouse1 = '1', mouse2 = '2', mouse3 = '3', mouse4 = '4', mouse5 = '5'} end
-local gamepad_to_button = {fdown = 'a', fup = 'y', fleft = 'x', fright = 'b', back = 'back', guide = 'guide', start = 'start',
-                           leftstick = 'leftstick', rightstick = 'rightstick', l1 = 'leftshoulder', r1 = 'rightshoulder',
-                           dpup = 'dpup', dpdown = 'dpdown', dpleft = 'dpleft', dpright = 'dpright'}
-local axis_to_button = {leftx = 'leftx', lefty = 'lefty', rightx = 'rightx', righty = 'righty', l2 = 'triggerleft', r2 = 'triggerright'}
-
-function Input:down(action)
-    for _, key in ipairs(self.binds[action]) do
-        if (love.keyboard.isDown(key) or love.mouse.isDown(key_to_button[key] or 0)) then
-            return true
-        end
-        
-        -- Supports only 1 gamepad, add more later...
-        if self.joysticks[1] then
-            if axis_to_button[key] then
-                return self.state[key]
-            elseif gamepad_to_button[key] then
-                if self.joysticks[1]:isGamepadDown(gamepad_to_button[key]) then
-                    return true
-                end
-            end
-        end
-    end
-end
-
-function Input:unbind(key)
-    for action, keys in pairs(self.binds) do
-        for i = #keys, 1, -1 do
-            if key == self.binds[action][i] then
-                table.remove(self.binds[action], i)
-            end
-        end
-    end
+    return true
+  end
+  return false
 end
 
 function Input:unbindAll()
-    self.binds = {}
+  self.binds = nil
+  self.binds = {["pressed"] = {["all"] = copy(self.ids)}, ["moved"] = {["all"] = copy(self.ids)}, ["released"] = {["all"] = copy(self.ids)}, ["held"] = {["all"] = copy(self.ids)}}
 end
 
-local copy = function(t1)
-    local out = {}
-    for k, v in pairs(t1) do out[k] = v end
-    return out
+function Input:changeState(gamestate)
+  if gamestate == nil or gamestate == "all" then
+    self.currentstate = "all"
+  else
+    self.currentstate = gamestate
+    for action, statelist in pairs(self.binds) do
+      if statelist[gamestate] == nil then
+        self.binds[action][gamestate] = copy(self.ids)
+      end
+    end
+  end
 end
 
-function Input:update()
-    self:pressed()
-    self.prev_state = copy(self.state)
-    self.state['wheelup'] = false
-    self.state['wheeldown'] = false
+local key_to_button = {mouse1 = '1', mouse2 = '2', mouse3 = '3', mouse4 = '4', mouse5 = '5'}
+local axis_to_button = {leftx = 'leftx', lefty = 'lefty', rightx = 'rightx', righty = 'righty', l2 = 'triggerleft', r2 = 'triggerright'}
+
+local function held()
+    local bindings = self.binds["held"][self.currentstate]
+    local modifier = self.currentmodifier
+    if bindings ~= nil then
+      for id, keylist in pairs(bindings) do
+        if keylist ~= nil then
+          for key, func in pairs(keylist) do
+            if modifier == nil then
+              if self.state[id][key] and key_to_button[key] == nil and axis_to_button[key] == nil then
+                bindings[id][key]()
+              end
+            else
+              if self.state[id][key] and self.state[id][modifier] then
+                bindings[id][modifier][key]()
+              end
+            end
+          end
+        end
+      end
+    end
 end
 
-function Input:keypressed(key)
-    self.state[key] = true
+function Input:update(dt)
+    held()
+    self.state[0]['wheelup'] = false
+    self.state[0]['wheeldown'] = false
+    self.state[0]['wheelleft'] = false
+    self.state[0]['wheelright'] = false
 end
 
-function Input:keyreleased(key)
-    self.state[key] = false
+function Input:keypressed(key, scancode, isrepeat)
+    local id = 0 --It's a keyboard
+    self.state[id][key] = true
+    setModifier(key)
+    executeFunction("pressed", id, key, self.currentmodifier, isrepeat)
 end
 
-local button_to_key = nil
-if love_version == '0.9.1' or love_version == '0.9.2' then
-    button_to_key = {l = 'mouse1', r = 'mouse2', m = 'mouse3', wu = 'wheelup', wd = 'wheeldown', x1 = 'mouse4', x2 = 'mouse5'}
-else button_to_key = {[1] = 'mouse1', [2] = 'mouse2', [3] = 'mouse3', [4] = 'mouse4', [5] = 'mouse5'} end
-
-function Input:mousepressed(x, y, button)
-    self.state[button_to_key[button]] = true
+function Input:keyreleased(key, scancode, isrepeat)
+    local id = 0 --It's a mouse
+    self.state[id][key] = false
+    if self.currentmodifer == key then setModifier(nil) end
+    executeFunction("released", id, key, self.currentmodifier, isrepeat)
 end
 
-function Input:mousereleased(x, y, button)
-    self.state[button_to_key[button]] = false
+local button_to_key = {[1] = 'mouse1', [2] = 'mouse2', [3] = 'mouse3', [4] = 'mouse4', [5] = 'mouse5'}
+
+function Input:mousepressed(x, y, button, istouch)
+    local id = 0 --It's a mouse
+    local key = button_to_key[button]
+    self.state[id][key] = true
+    executeFunction("pressed", id, key, self.currentmodifier, x, y, button, istouch)
+end
+
+function Input:mousereleased(x, y, button, istouch)
+    local id = 0 --It's a mouse
+    local key = button_to_key[button]
+    self.state[id][key] = false
+    executeFunction("released", id, key, self.currentmodifier, x, y, button, istouch)
+end
+
+-- Assumes you only want to do this when a key is pressed on the mouse.
+-- Could technically work for all keys but it would pick up things such
+-- as wheelup and wheeldown which we don't want.
+function Input:mousemoved(x, y, dx, dy)
+    local id = 0 --It's a mouse
+    for k, key in pairs(button_to_key) do
+      if self.state[id][key] == true then
+        executeFunction("held", id, key, self.currentmodifier, x, y, dx, dy)
+      else
+        executeFunction("moved", id, key, self.currentmodifier, x, y, dx, dy)
+      end
+    end
 end
 
 function Input:wheelmoved(x, y)
-    if y > 0 then self.state['wheelup'] = true
-    elseif y < 0 then self.state['wheeldown'] = true end
+    local id = 0 --It's a mouse
+    local key = nil
+    if y > 0 then 
+      key = 'wheelup'
+    elseif y < 0 then 
+      key = 'wheeldown'
+    end
+    if key ~= nil then
+      self.state[id][key] = true
+      executeFunction("moved", id, key, self.currentmodifier, y)
+    end
+    key = nil
+    if x > 0 then 
+      key = 'wheelleft'
+    elseif x < 0 then 
+      key = 'wheelright'
+    end
+    if key ~= nil then
+      self.state[id][key] = true
+      executeFunction("moved", id, key, self.currentmodifier, x)
+    end
 end
 
 local button_to_gamepad = {a = 'fdown', y = 'fup', x = 'fleft', b = 'fright', back = 'back', guide = 'guide', start = 'start',
                            leftstick = 'leftstick', rightstick = 'rightstick', leftshoulder = 'l1', rightshoulder = 'r1',
                            dpup = 'dpup', dpdown = 'dpdown', dpleft = 'dpleft', dpright = 'dpright'}
+local gamepad_to_button = {fdown = 'a', fup = 'y', fleft = 'x', fright = 'b', back = 'back', guide = 'guide', start = 'start',
+                           leftstick = 'leftstick', rightstick = 'rightstick', l1 = 'leftshoulder', r1 = 'rightshoulder',
+                           dpup = 'dpup', dpdown = 'dpdown', dpleft = 'dpleft', dpright = 'dpright'}
+local button_to_axis = {leftx = 'leftx', lefty = 'lefty', rightx = 'rightx', righty = 'righty', triggerleft = 'l2', triggerright = 'r2'}
+
+function Input:joystickadded(joystick)
+  updateJoysticks()
+end
+
+function Input:joystickremoved(joystick)
+  updateJoysticks()
+end
 
 function Input:gamepadpressed(joystick, button)
-    self.state[button_to_gamepad[button]] = true 
+    local id = self.joysticks_to_ids[joystick:getID()]
+    local key = button_to_gamepad[button]
+    self.state[id][key] = true
+    executeFunction("pressed", id, key, nil)
 end
 
 function Input:gamepadreleased(joystick, button)
-    self.state[button_to_gamepad[button]] = false
+    local id = self.joysticks_to_ids[joystick:getID()]
+    local key = button_to_gamepad[button]
+    self.state[id][key] = false
+    executeFunction("released", id, key, nil)
 end
 
-local button_to_axis = {leftx = 'leftx', lefty = 'lefty', rightx = 'rightx', righty = 'righty', triggerleft = 'l2', triggerright = 'r2'}
 
-function Input:gamepadaxis(joystick, axis, newvalue)
-    self.state[button_to_axis[axis]] = newvalue
+function Input:gamepadaxis(joystick, axis, value)
+    local id = self.joysticks_to_ids[joystick:getID()]
+    local key = button_to_axis[axis]
+    if self.joystick_axis_values[key] ~= nil and self.joystick_axis_values[key] == value then
+      executeFunction("held", id, key, nil, value)
+    else
+      executeFunction("moved", id, key, nil, value)
+    end
+    self.joystick_axis_values[key] = value
 end
 
-return setmetatable({new = new}, {__call = function(_, ...) return Input.new(...) end})
+  return setmetatable(Input,{
+    __call = function(self,...)
+      return self:new(...)
+    end
+  })
